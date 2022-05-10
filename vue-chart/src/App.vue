ed0473f915f49d84dc6ded9444ac34275588c657
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2022-05-10 21:33:07 +0900

    13. 뷰 플러그인과 외부 라이브러리 타입 정의

--- vue-chart/src/App.vue
+++ vue-chart/src/App.vue
@@ -4,33 +4,35 @@
   </div>
 </template>
 <script lang="ts">
-import Vue from "vue";
-import { ChartConfiguration } from "chart.js";
 import Chart from "chart.js/auto";
+import Vue from "vue";
 
 export default Vue.extend({
   mounted() {
-    const labels = ["January", "February", "March", "April", "May", "June"];
-    const data = {
-      labels: labels,
-      datasets: [
-        {
-          label: "My First dataset",
-          backgroundColor: "rgb(255, 99, 132)",
-          borderColor: "rgb(255, 99, 132)",
-          data: [0, 10, 5, 2, 20, 30, 45],
-        },
-      ],
-    };
-    const config: ChartConfiguration = {
+    const canvasElement = document.getElementById(
+      "myChart"
+    ) as HTMLCanvasElement;
+
+    const ctx = canvasElement.getContext("2d");
+    if (ctx === null) {
+      return;
+    }
+
+    const myChart = new Chart(ctx, {
       type: "line",
-      data,
+      data: {
+        labels: ["January", "February", "March", "April", "May", "June"],
+        datasets: [
+          {
+            label: "My First dataset",
+            backgroundColor: "rgb(255, 99, 132)",
+            borderColor: "rgb(255, 99, 132)",
+            data: [0, 10, 5, 2, 20, 30, 45],
+          },
+        ],
+      },
       options: {},
-    };
-    const ctx = (
-      document.getElementById("myChart") as HTMLCanvasElement
-    ).getContext("2d") as CanvasRenderingContext2D;
-    var myChart = new Chart(ctx, config);
+    });
   },
 });
 </script>
